cb81fca1d8dd5110431bc091f1cf1246ec12f2af
Manish Chand <manishchandr349@gmail.com>
2023-10-31 11:59:01 +0530

    adding

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -4,7 +4,12 @@ import phone from "../assets/Contact-img/24-hours.webp";
 import { BsFillStarFill } from "react-icons/bs";
 import { CiMail, CiMobile3, CiLocationOn } from "react-icons/ci";
 
+
+
+
 export const ContactUs = () => {
+
+  
   return (
     <>
       <Main>
@@ -53,7 +58,11 @@ export const ContactUs = () => {
               </div>
             </div>
           </section>
+       
 
+       <div className="container">
+
+       
           <section className="section-get-in">
             <h1 className="heading-get-in">Get in Touch</h1>
             <p className="para">Give us ring. You can even come to meet us</p>
@@ -92,9 +101,76 @@ export const ContactUs = () => {
 
           <section className="contact-form">
             <div>
-              
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3503.530741038066!2d77.31074431555618!3d28.583850693032392!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce91a83de9ab3%3A0xd1ca78f64f373b2!2sCollege%20Vidya!5e0!3m2!1sen!2sin!4v1665561870569!5m2!1sen!2sin"
+                style={{ width: "100%", height: "100%" }}
+                frameborder="0"
+                loading="lazy"
+              ></iframe>
+            </div>
+            <div>
+              <h2>Contact Us</h2>
+              <form onSubmit="">
+                <div>
+                  <div className="gender-box">
+                    <div>
+                      <input className="input-radio" type="radio" id="male" name="gender" />
+                      <label htmlFor="male">Male</label>
+                    </div>
+                    <div>
+                      <input className="input-radio" type="radio" id="female" name="gender" />
+                      <label htmlFor="male">Female</label>
+                    </div>
+                  </div>
+
+                 
+                  <input
+                className="name-input"
+                    type="text"
+                    name="name"
+                    value=""
+                    onChange=""
+                    required
+                    placeholder="Name"
+                  />
+                </div>
+                <div>
+                  
+                  <input
+                  className="name-input"
+                    type="text"
+                    autoComplete="off"
+                    placeholder="Date of Birth (DD/MM/YYYY)" inputmode="numeric"
+                    value=""
+                    onChange=""
+                    required
+                  />
+                </div>
+                <div>
+                  <label>Message:</label>
+                  <textarea name="message" value="" onChange="" required />
+                </div>
+
+                <div>
+                  <button type="submit">Submit</button>
+                </div>
+              </form>
             </div>
           </section>
+
+          <section className="visit-Us">
+                <div>  
+                <h1>Call doesn't feel <br /> enough <span>Visit Us</span></h1>              
+                <p>At GoCollege, it is our constant endeavour to provide great customer experience. In case you require assistance, we have created multiple ways to reach out to us</p>
+                <button className="call-btn">Call Now</button>
+                <p><span>icone</span> Visit Us: (10 AM to 7 PM)</p>
+                <h3>C-1234 A block Sector 45, Noida, UK 20123</h3>
+                </div>
+                <div>  
+                <img src="" alt="" />              
+                </div>
+          </section>
+          </div>
         </div>
       </Main>
     </>
@@ -131,6 +207,10 @@ const Main = styled.div`
           padding-top: 10px;
           padding-left: 70px;
         }
+        .container{
+          max-width: 90%;
+          margin: auto;
+        }
         .box-2{
           display: flex;
           justify-content: space-around;
@@ -154,10 +234,7 @@ const Main = styled.div`
           font-size: 1rem;
           margin-top: 20px !important;
         }
-          .section-get-in{
-            max-width: 90%;
-            margin: auto;
-           }
+          
           .heading-get-in{
             margin-top: 50px;
             text-align: center;
@@ -195,5 +272,28 @@ const Main = styled.div`
           font-size: 1.3rem;
           color: blue;
           font-weight: 900;
+         }
+
+         .contact-form{
+
+         
+          margin-top: 50px;
+        display: grid;
+        grid-template-columns: 50% 50%;
+        gap: 40px;
+         }
+         .gender-box{
+          display: flex;
+          margin: 20px 0;
+        gap: 10px;
+         }
+         .input-radio{
+          width: 20px;
+         }
+         .name-input{
+          padding: 15px 200px 15px 10px;
+          border-radius: 10px;
+          font-size: 1.2rem;
+          margin-bottom: 10px;
          }
        `;
